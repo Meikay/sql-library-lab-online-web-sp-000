06c88ac6bfbb381e15b85107b09b8a272b77dbad
Meisuleen Kased <meisuleen@gmail.com>
2019-02-25 23:10:59 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT author_id, subgenre_id FROM series
+  "SELECT authors.name AS author_name, subgenres.name AS subgenre_name FROM series
      INNER JOIN authors
      ON series.author_id = authors.id
      INNER JOIN subgenres
@@ -29,7 +29,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT characters, COUNT(characters) FROM series
+   GROUP BY characters
+   ORDER BY COUNT(characters) DESC
+   LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
